b8d55a2fe07f8629f0d7d6a6e7b3bf4bdc7f3c15
Xin Cheng Shen <shex7219589@gmail.com>
2023-06-21 21:23:12 -0400

    connect db

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+exports.connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_DB_URL, {});
+    console.log("DB SUCCESSFULLY CONNECTED");
+  } catch (err) {
+    console.log("DB CONNECTION FAILED...");
+    console.error(error);
+    process.exit(1);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 
+const { connectDB } = require("./config/database");
+
 const port = process.env.PORT || 8080;
 
 const app = express();
@@ -9,6 +11,11 @@ app.get("/", (_, res) => {
   res.send("FarmWise Backend Server");
 });
 
-app.listen(port, () => {
-  console.log(`APP RUNNING ON PORT ${port}`);
-});
+const startApp = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`APP RUNNING ON PORT ${port}`);
+  });
+};
+
+startApp();

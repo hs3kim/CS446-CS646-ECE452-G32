3c566cc399b79ef5d4abf569be9a6dd1eebb7a7e
Xin Cheng Shen <shex7219589@gmail.com>
2023-06-23 20:48:02 -0400

    Authentication Endpoints (#2)
    * add dependencies
    
    * connect db
    
    * user schema
    
    * register
    
    * login user
    
    * add auth middleware

---
+++ backend/services/authService.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const { User } = require("../models/user");
+const { AppError } = require("../utils/errors");
+
+exports.checkDuplicateUser = async (username) => {
+  const user = await User.findOne({ username });
+  if (user) {
+    throw new AppError("DUPLICATE_RECORD", "Username Already in Use");
+  }
+};
+
+exports.createUser = async (username, email, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email: email.toLowerCase(),
+    password: hashedPassword,
+  });
+
+  user.password = "";
+  return user;
+};
+
+exports.createJWTSignature = (user) => {
+  const token = jwt.sign(
+    {
+      userID: user._id,
+      username: user.username,
+      email: user.email,
+    },
+    process.env.JWT_TOKEN_KEY
+  );
+  return token;
+};
+
+exports.loginUser = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new AppError("BAD_REQUEST", "User Does Not Exist");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    throw new AppError("BAD_REQUEST", "Invalid Username or Password");
+  }
+
+  user.password = "";
+  return user;
+};

d7657acefbcd2dbefa4a80bc872f4731469e48cb
Han Su <hs3kim@uwaterloo.ca>
2023-06-24 14:07:29 -0400

    Bottom nav bar

---
+++ frontend/FarmWise/app/src/main/java/com/example/farmwise/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.farmwise;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        replaceFragment(new HomeFragment());
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.farm){
+                replaceFragment(new FarmFragment());
+            } else if (id == R.id.market){
+                replaceFragment(new MarketFragment());
+            } else if (id == R.id.charity){
+                replaceFragment(new CharityFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}

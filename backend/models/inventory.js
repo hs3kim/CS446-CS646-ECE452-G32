5e16bf8517c151ff0b2a2ab982075049f1ad12cb
j53meng <134098325+j53meng@users.noreply.github.com>
2023-06-25 13:17:54 -0400

    Create inventory.js

---
+++ backend/models/inventory.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const farmSchema = new mongoose.Schema({
+    code: {
+        type: Number,
+        unique: true,
+    },
+    name: String,
+});
+
+const cropInventorySchema = new mongoose.Schema({
+    farm: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Farm'
+    },
+    product: String,
+    count: Number
+});
+
+cropInventorySchema.index({ farm: 1, product: 1 }, { unique: true });
+
+const Farm = mongoose.model('Farm', farmSchema);
+const cropInventory = mongoose.model('cropInventory', cropInventorySchema);
+
+module.exports = { Farm };
+module.exports = { cropInventory };

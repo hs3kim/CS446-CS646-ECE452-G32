829f605d2392a070a01d67a16005d503580ebdbf
Han Su <hs3kim@uwaterloo.ca>
2023-07-23 13:43:15 -0400

    -Add internet access -Integrate volley library -Dynamically load farm cards in home page calling endpoints

--- frontend/FarmWise/app/src/main/java/com/example/farmwise/HomeFragment.java
+++ frontend/FarmWise/app/src/main/java/com/example/farmwise/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.farmwise;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +17,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.farmwise.databinding.FragmentHomeBinding;
 import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -74,6 +92,69 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(getLayoutInflater(), container, false);
         View view = binding.getRoot();
+
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString("10005", "FarmNameInSharedPref");
+//        editor.commit();
+
+        // Get list of farms
+        String reqURL = "https://farmwise.onrender.com/api/user/get";
+
+        TextView farmListText = view.findViewById(R.id.farmList);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, reqURL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String status;
+                        JSONObject data;
+                        try{
+                            status = response.getString("status");
+                            if (status.equals("SUCCESS")) {
+                                data = response.getJSONObject("data");
+
+                                // create farm cards for owned farms
+                                JSONArray ownedFarms = data.getJSONArray("owns");
+                                for (int i = 0; i < ownedFarms.length(); i++){
+                                    JSONObject farm = ownedFarms.getJSONObject(i);
+                                    String farmCode = farm.getString("code");
+                                    String farmName = sharedPreferences.getString(farmCode, farm.getString("name"));
+                                    addFarm(farmName, farmCode);
+                                }
+
+                                // create farm cards for farms working at
+                                JSONArray worksAtFarms = data.getJSONArray("worksAt");
+                                for (int i = 0; i < worksAtFarms.length(); i++){
+                                    JSONObject farm = worksAtFarms.getJSONObject(i);
+                                    String farmCode = farm.getString("code");
+                                    String farmName = sharedPreferences.getString(farmCode, farm.getString("name"));
+                                    addFarm(farmName, farmCode);
+                                }
+                            }
+                        }
+                        catch (JSONException e) {
+                            status = "error parsing JSON";
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+//                        farmListText.setText("error");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap header = new HashMap();
+                header.put("Content-Type", "application/json");
+                header.put("Cookie", "");
+                return header;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(jsonObjectRequest);
+
 
         buildDialog();
 

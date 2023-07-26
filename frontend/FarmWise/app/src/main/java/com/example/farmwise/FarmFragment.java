789b698f6a72d7c82d2e5eef73a5dfbec8a43b6c
billycz8 <56795933+billycz8@users.noreply.github.com>
2023-07-26 12:35:54 -0400

    Update crops record to backend, sends the api post request to update inventory (#16)
    * moved everthing from other branch to prevent merge history conflict
    
    * added button to check internet connection and upload recognized crops while offline to backend if there is connection
    
    * added .gitignore for build directory
    
    * added .gitignore
    
    * added .gitignore
    
    * removed build files
    
    * removed commented out files
    
    * added files message text for internet connection or upload successful
    
    * removed build files and added gitignore
    
    ---------

--- frontend/FarmWise/app/src/main/java/com/example/farmwise/FarmFragment.java
+++ frontend/FarmWise/app/src/main/java/com/example/farmwise/FarmFragment.java
@@ -3,16 +3,51 @@ package com.example.farmwise;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
+
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import com.example.farmwise.PocketSphinxActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +64,8 @@ public class FarmFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView statusTextView;
 
     public FarmFragment() {
         // Required empty public constructor
@@ -67,6 +104,10 @@ public class FarmFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_farm, container, false);
 
+
+        // Find the TextView by its ID
+        statusTextView = view.findViewById(R.id.statusTextView);
+
         // Find the button by its ID
         Button crop_recognition_button = view.findViewById(R.id.farmbutton);
 
@@ -78,7 +119,161 @@ public class FarmFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        Button upload_button = view.findViewById(R.id.uploadbutton);
+
+        upload_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // upload
+                if (isConnectedToInternet()) {
+                    List<String> recognizedWordsList = updateRecognizedWordsToBackend();
+                    sendToBackend(recognizedWordsList);
+                    clearRecognizedCropsFile();
+                } else {
+                    showStatusMessage("Please connect to internet first");
+                }
+            }
+        });
+
         return view;
 //        return inflater.inflate(R.layout.fragment_farm, container, false);
+    }
+    private void showStatusMessage(String message) {
+        if (statusTextView != null) {
+            statusTextView.setText(message);
+            statusTextView.setVisibility(View.VISIBLE);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    hideStatusMessage();
+                }
+            }, 1500);
+        }
+    }
+    private void hideStatusMessage() {
+        if (statusTextView != null) {
+            statusTextView.setVisibility(View.GONE);
+        }
+    }
+    private void sendToBackend(List<String> recognizedWordsList) {
+        // Get list of farms
+        String reqURL = "https://farmwise.onrender.com/api/inventory/update";
+        try {
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("farmCode", 10005);
+            JSONArray textsArray = new JSONArray(recognizedWordsList);
+            requestBody.put("texts", textsArray);
+            final String mRequestBody = requestBody.toString();
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.POST, reqURL, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println(response);
+                            String status;
+                            JSONObject data;
+                            try{
+                                status = response.getString("status");
+                                if (status.equals("SUCCESS")) {
+                                    clearRecognizedCropsFile();
+                                    showStatusMessage("Upload successful");
+                                }
+                            }
+                            catch (JSONException e) {
+                                status = "error parsing JSON";
+                                System.out.println(status);
+                            } catch (Exception e) {
+                                System.out.println("error");
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println("response error");
+                        }
+                    }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap header = new HashMap();
+                    header.put("Content-Type", "application/json");
+                    header.put("Cookie", "");
+                    return header;
+                }
+                @Override
+                public byte[] getBody() {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    }
+                    catch (UnsupportedEncodingException uee) {
+                        return null;
+                    }
+                }
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+            System.out.println("Adding request");
+            RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+            requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+    private boolean isConnectedToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            Network network = connectivityManager.getActiveNetwork();
+            if (network != null) {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+                return networkCapabilities != null &&
+                        (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET));
+            }
+        }
+        return false;
+    }
+    private List<String> updateRecognizedWordsToBackend() {
+        List<String> recognizedWordsList = new ArrayList<>();
+        String filename = "recognized_crops.txt";
+        File file = new File(getActivity().getExternalFilesDir(null), filename);
+
+        if (file.exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    recognizedWordsList.add(line);
+                    System.out.println(line); // Print each line to the console
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("recognized_words.txt does not exist.");
+        }
+        System.out.println(recognizedWordsList);
+        return recognizedWordsList;
+    }
+    private void clearRecognizedCropsFile() {
+        String filename = "recognized_crops.txt";
+        File file = new File(getActivity().getExternalFilesDir(null), filename);
+
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(""); // Write an empty string to truncate the file
+            fileWriter.close();
+            System.out.println("Cleared contents of recognized_crops.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
